047680d52177013b0db544a701d0e96fc564ab9c
Ved Tiwari <ved@webkorps.com>
2022-09-10 01:26:54 +0530

    added a new column (password_digest) and worked on it

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 20}
     validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 6, maximum: 30}
     validates :gender, presence: true, length: { maximum: 6}
+    has_secure_password
 end
