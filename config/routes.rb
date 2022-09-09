4f492d7c8912c3eac7bc8a049ecf17165bfe9dfe
Ved Tiwari <ved@webkorps.com>
2022-09-09 21:05:54 +0530

    add controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/create'
+  get 'users/show'
+  get 'users/update'
+  get 'users/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
