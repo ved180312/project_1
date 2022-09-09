3a5743ac615eedeee121d18a12ca4fc35ca589a6
Ved Tiwari <ved@webkorps.com>
2022-09-09 22:16:38 +0530

    Added validations to the model and all user functionality (view)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+    validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 20}
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 6, maximum: 30}
+    validates :gender, presence: true, length: { maximum: 1}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/create'
-  get 'users/show'
-  get 'users/update'
-  get 'users/new'
+  # get 'users/index'
+  # get 'users/create'
+  # get 'users/show'
+  # get 'users/update'
+  # get 'users/new'
+
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
