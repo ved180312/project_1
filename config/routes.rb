2bad2cebaeb76741c71959e16bc490c343b71de8
Ved Tiwari <ved@webkorps.com>
2022-09-12 18:20:29 +0530

    added user login logout form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
   # get 'users/index'
   # get 'users/create'
   # get 'users/show'
@@ -6,6 +9,12 @@ Rails.application.routes.draw do
   # get 'users/new'
 
   resources :users
+
+
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
